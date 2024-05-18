58ffa3657bfa537cdcc19c826b3975a59f7febe3
azraisleyen <zrisleyen@gmail.com>
2024-05-18 21:51:27 +0300

    many-to-one ilişkisi kurulup tarif ve egzersiz eklendi

--- Proje/pages/admin.py
+++ Proje/pages/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Recipe
 from .models import Category
+from .models import Workout
 
 admin.site.register(Category)
 admin.site.register(Recipe)
+admin.site.register(Workout)
